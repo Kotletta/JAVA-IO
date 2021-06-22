97bfd435b595670f5d9af5a3293fe7c7c6a41536
Kotletta <kotletta@internet.ru>
2021-06-22 07:34:34 +0300

    Added solution two optional task

---
+++ src/OptionalTask/Task2/publicReplacePrivate.java
@@ -0,0 +1,27 @@
+package OptionalTask.Task2;
+
+import java.io.*;
+
+public class publicReplacePrivate {
+    public static void main(String[] args) {
+        {
+            try (FileWriter writer = new FileWriter("data/SomeJavaCodeReplace.txt");
+                 BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                 FileReader reader = new FileReader("data/SomeJavaCode.txt");
+                 BufferedReader bufferedReader = new BufferedReader(reader)) {
+                 String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    line = line.replaceAll("public", "private");
+                    bufferedWriter.write(line, 0, line.length());
+                    bufferedWriter.newLine();
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
